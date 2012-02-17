07ed93aca1d6189bd6aa45e16e84c09613ada0cb
Hamid <hamid@codebiztech.com>
2012-02-17 17:51:43 +0500

    relased

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,3 +1,3 @@
-$connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-$db = $connection.db(ENV['MONGOHQ_URL'] =~ /([^\/]+)$/ ? $1 : 
-raise("Please include your db with your MONGOHQ_URL : #{ENV['MONGOHQ_URL'])"))
+#$connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+#$db = $connection.db(ENV['MONGOHQ_URL'] =~ /([^\/]+)$/ ? $1 : 
+#raise("Please include your db with your MONGOHQ_URL : #{ENV['MONGOHQ_URL'])"))
